bb377fe2a401b017dfb8db2e00499fdde4d71018
carlos210403 <111439558+carlos210403@users.noreply.github.com>
2022-08-22 08:51:10 -0500

    First Python Program

---
+++ First.py
@@ -0,0 +1,6 @@
+# Programmer: Carlos Suarez
+# Program : Hello World, Following Tradition
+# Pyhton 3.7.9
+# First Writen : 8-22-2022, 8:46:00 AM
+# Last Updated : 8-22-2022, 8:46:00 AM
+print ("hello world")
