c899f685b81b524985a4329e9c4d6dc571ef541d
carlos210403 <111439558+carlos210403@users.noreply.github.com>
2022-08-29 08:49:33 -0500

    Arithemetic Operations 1

---
+++ arithemetic.py
@@ -0,0 +1,15 @@
+# Programmer : Carlos Suarez
+# Program : Arithemtic Operations
+# First Created : 08/29/2022 08:32:00
+# Last Changed :  08/29/2022 08:32:00
+# Version : 3.7.9
+
+print("Arithmetic Operations")
+firstInput = 5
+secondInput = 10
+
+print("sum of given numbers is ", firstInput+secondInput)
+print("difference of given number is ", firstInput-secondInput)
+print("remainder of given number is ", firstInput*secondInput)
+print("quotiant of given number is ", firstInput/secondInput)
+print("product of given number is  ", firstInput%secondInput)
