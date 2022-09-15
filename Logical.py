f6d778c8ed768cb10301cf446a9424fbe9a6a29e
carlos210403 <111439558+carlos210403@users.noreply.github.com>
2022-09-15 17:55:34 -0500

    Logical Operations

---
+++ Logical.py
@@ -0,0 +1,14 @@
+# Programmer : Carlos Suarez
+# Program : Logical Operations
+# First Created : 09/08/2022 08:20:00
+# Last Changed :  09/09/2022 12:15:00
+# Version : 3.7.9
+
+print("Logical Operations")
+
+
+print('x = 21')
+
+print("'and' Ex. x>5 x<25")
+print("'or' Ex. x<25 x<12")
+print("'not' Ex. not(x>5 x<25")
