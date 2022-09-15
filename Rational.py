dfc722bb2e2cb6ae02a066adefb2b02cbe6e37a7
carlos210403 <111439558+carlos210403@users.noreply.github.com>
2022-09-15 17:06:20 -0500

    Add files via upload

---
+++ Rational.py
@@ -0,0 +1,15 @@
+# Programmer : Carlos Suarez
+# Program : Rational Operations
+# First Created : 09/08/2022 08:20:00
+# Last Changed :  09/09/2022 11:32:00
+# Version : 3.7.9
+
+print("Rational Operations")
+
+print("'<' = Less than Ex. 2<7")
+print("'>' = Greater Than Ex. 7>2")
+print("'<=' = Less than or equal to Ex. 2<=7")
+print("'>=' = Greater Than or Equal to Ex. 7>=2")
+print("'==' = Equal to Ex. 7==7")
+print("'!=' = Not Equal to Ex. 7!=2")
+
