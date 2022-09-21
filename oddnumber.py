f04e9ad73dee519607535a9c83411bbf198a3046
Carlos Suarez Salazar <carlos.suarezsalazar@sckans.edu>
2022-09-21 08:27:50 -0500

    odd number program

---
+++ oddnumber.py
@@ -0,0 +1,11 @@
+# Programmer : Carlos Suarez
+# Program : Odd Numbers
+# First Created : 09/21/2022 08:20:00
+# Last Changed :  09/21/2022 11:32:00
+# Version : 3.7.9
+
+print("Printing Odd Number from First 100 Natural Numbers")
+
+for i in range (0, 101):
+    if (i%2 != 0):
+        print(i, end=' ')
