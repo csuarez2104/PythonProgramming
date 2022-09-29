292dcc45a493ad549957bd0bdba3e6beba5b9c1f
carlos210403 <111439558+carlos210403@users.noreply.github.com>
2022-09-29 15:13:27 -0500

    Add files via upload

---
+++ timerProject.py
@@ -0,0 +1,10 @@
+print("Welcome Please Input your Time")
+time =int(input("Please Enter your time: "))
+print(time)
+
+if time <= 700:
+    print("Invalid Time")
+elif time <= 1700:
+    print("Enjoy Working")
+if time >= 1700:
+    print("Go Home!")
