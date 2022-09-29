6aedb7282592277d1707cea3f1f5910170e29c2a
Carlos Suarez Salazar <carlos.suarezsalazar@sckans.edu>
2022-09-29 15:15:09 -0500

    Odd numbers loops

--- oddnumber.py
+++ oddnumber.py
@@ -4,19 +4,19 @@
 # Last Changed :  09/21/2022 11:32:00
 # Version : 3.7.9
 
-# print("Printing Odd Number from First 100 Natural Numbers")
-#
-# for i in range (0, 101):
-#     if (i%2 != 0):
-#         print(i, end=' ')
+print("Printing Odd Number from First 100 Natural Numbers")
+
+for i in range (0, 501):
+    if (i%2 != 0):
+        print(i, end=' ')
 #
 # print("Alternate Approach")
 # for k in range(1, 51):
 #     print(((2*k)-1), end=' ')
 
-print("Printing Odd Number from First 100 Naturals Numbers while using loop")
-i=100
-while i >= 1:
-    if i%2!=0:
-        print(i, end=' ')
-    i=i-1
+# print("Printing Odd Number from First 100 Naturals Numbers while using loop")
+# i=100
+# while i >= 1:
+#     if i%2!=0:
+#         print(i, end=' ')
+#     i=i-1
