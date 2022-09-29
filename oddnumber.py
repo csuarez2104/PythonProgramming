83628a629177ae6760d973b4590b934487609123
Carlos Suarez Salazar <carlos.suarezsalazar@sckans.edu>
2022-09-29 15:37:59 -0500

    100 TO 200 ODD

--- oddnumber.py
+++ oddnumber.py
@@ -4,14 +4,14 @@
 # Last Changed :  09/21/2022 11:32:00
 # Version : 3.7.9
 
-print("Printing Odd Number from First 100 Natural Numbers")
-
-for i in range (0, 501):
-    if (i%2 != 0):
-        print(i, end=' ')
+# print("Printing Odd Number from First 100 Natural Numbers")
 #
+# for i in range (0, 501):
+#     if (i%2 != 0):
+# #         print(i, end=' ')
+# #
 # print("Alternate Approach")
-# for k in range(1, 51):
+# for k in range(100, 201):
 #     print(((2*k)-1), end=' ')
 
 # print("Printing Odd Number from First 100 Naturals Numbers while using loop")
@@ -20,3 +20,8 @@ for i in range (0, 501):
 #     if i%2!=0:
 #         print(i, end=' ')
 #     i=i-1
+print("Printing Odd Number from between 100  and 200 Natural Numbers")
+
+for i in range (100, 200):
+    if (i%2 != 0):
+        print(i, end=' ')
